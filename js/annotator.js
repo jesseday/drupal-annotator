35d51307361b85db776a8d86b1064b9e00ead4eb
Jesse Day <09jday@gmail.com>
2013-04-08 15:33:13 -0400

    Permissions and general settings updates.
    Adds permissions for viewing all and viewing own annotations.
    Adds a general settings admin page.
    Adds options for adding the annotator based on the node type.
    Adds field for specifying the selector the annotator should be instantiated
    on.

--- js/annotator.js
+++ js/annotator.js
@@ -1,7 +1,26 @@
 (function ($) {
   Drupal.behaviors.annotator = {
     attach: function (context, settings) {
-      Drupal.Annotator = $('.node .content').annotator();
+
+      var selector = (Drupal.settings.annotator_selector) ?
+          Drupal.settings.annotator_selector : '.content';
+
+      if(Drupal.settings.annotator_enabled_node_types) {
+          var enabled = '';
+          var count = Drupal.settings.annotator_enabled_node_types.length - 1;
+          Drupal.settings.annotator_enabled_node_types.forEach(function(name, index) {
+              enabled += name + ' ' + selector;
+              enabled += (index < count) ? ', ' : '';
+          });
+
+          Drupal.Annotator = $(enabled).annotator();
+
+      }
+      else {
+          selector = '.node ' + selector;
+          Drupal.Annotator = $(selector).annotator();
+      }
+
     }
   };
 })(jQuery);
