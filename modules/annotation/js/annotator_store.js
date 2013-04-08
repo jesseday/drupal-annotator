c3efb9920eb01a6eb7c1876a32a8330e93c093a5
Jesse Day <09jday@gmail.com>
2013-04-08 15:44:07 -0400

    Makes load from search optional.

--- modules/annotation/js/annotator_store.js
+++ modules/annotation/js/annotator_store.js
@@ -1,6 +1,10 @@
 (function ($) {
   Drupal.behaviors.annotatorStore = {
     attach: function (context, settings) {
+      var loadSearch = (settings.annotator_store.toggle_search) ? 
+        {'limit': 0,'uri': window.location.href} : false;
+      var id = $('.node');
+
       Drupal.Annotator.annotator('addPlugin', 'Store', {
         prefix: settings.annotator_store.prefix,
         urls: settings.annotator_store.urls,
@@ -9,10 +13,8 @@
         annotationData: {
           'uri': window.location.href
         },
-        loadFromSearch: {
-          'limit': 0,
-          'uri': window.location.href
-        }
+        loadFromSearch: loadSearch
+
       });
     }
   };
